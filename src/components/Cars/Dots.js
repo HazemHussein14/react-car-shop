332741890c68d38c67c4d33ecedc229751293bee
Hazem Hussein <113997542+HazemHussein14@users.noreply.github.com>
2023-05-16 14:41:21 +0300

    removed aria attribute causing problems

--- src/components/Cars/Dots.js
+++ src/components/Cars/Dots.js
@@ -2,7 +2,6 @@ const CustomDot = ({ onClick, active }) => {
   return (
     <li className="mr-5">
       <button
-        aria-label="Change slide"
         className={`w-3 h-3 bg-[#eee] rounded-[50%] cursor-pointer ${
           active ? "bg-[rgb(116,25,6)]" : "bg-[#eee]"
         }`}
