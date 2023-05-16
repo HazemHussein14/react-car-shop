4983878757aaebcaf2d95834d6bb5cc18610d440
Hazem Hussein <113997542+HazemHussein14@users.noreply.github.com>
2023-05-16 10:32:50 +0300

    fixed accessabilty issue in dots component

--- src/components/Cars/Dots.js
+++ src/components/Cars/Dots.js
@@ -2,6 +2,7 @@ const CustomDot = ({ onClick, active }) => {
   return (
     <li className="mr-5">
       <button
+        aria-label="Change slide"
         className={`w-3 h-3 bg-[#eee] rounded-[50%] cursor-pointer ${
           active ? "bg-[rgb(116,25,6)]" : "bg-[#eee]"
         }`}
